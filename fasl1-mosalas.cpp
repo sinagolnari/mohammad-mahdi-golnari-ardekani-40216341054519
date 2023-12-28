eb5fc2bab4560f96479041e81e301c967876191c
sinagolnari <151348915+sinagolnari@users.noreply.github.com>
2023-12-28 23:50:22 +0330

    Add files via upload

---
+++ fasl1-mosalas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int (a ,b ,c) {
+    if (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a);
+}
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout <<"area is:"<< a + b + c;
+    return 0;
+}
---
+++ fasl1-sum.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    long int a,b; 
+    cin>>a;
+    cin>>b;
+    cout<<"sum is"<<"\n (a + b)";
+    return 0;
+}
